8062a053efaf2e5f36b3c174fe0404e2421be788
Utsav <das.utsav1995@gmail.com>
2016-09-04 22:33:43 +0530

    Added Action bar menu

--- app/src/main/java/com/example/lenovo/game/MainActivity.java
+++ app/src/main/java/com/example/lenovo/game/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,37 +35,54 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    rolledNo = r.nextInt(6)+1;
+                rolledNo = r.nextInt(6) + 1;
 
-                if(rolledNo == 1){
+                if (rolledNo == 1) {
                     imageView.setImageResource(R.drawable.a1);
-                    Toast.makeText(MainActivity.this, "One",Toast.LENGTH_SHORT).show();
-                }
-                else if(rolledNo == 2){
+                    Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
+                } else if (rolledNo == 2) {
                     imageView.setImageResource(R.drawable.a2);
-                    Toast.makeText(MainActivity.this, "Two",Toast.LENGTH_SHORT).show();
-                }
-                else if(rolledNo == 3){
+                    Toast.makeText(MainActivity.this, "Two", Toast.LENGTH_SHORT).show();
+                } else if (rolledNo == 3) {
                     imageView.setImageResource(R.drawable.a3);
-                    Toast.makeText(MainActivity.this, "Three",Toast.LENGTH_SHORT).show();
-                }
-                else if(rolledNo == 4){
+                    Toast.makeText(MainActivity.this, "Three", Toast.LENGTH_SHORT).show();
+                } else if (rolledNo == 4) {
                     imageView.setImageResource(R.drawable.a4);
-                    Toast.makeText(MainActivity.this, "Four",Toast.LENGTH_SHORT).show();
-                }
-                else if(rolledNo == 5){
+                    Toast.makeText(MainActivity.this, "Four", Toast.LENGTH_SHORT).show();
+                } else if (rolledNo == 5) {
                     imageView.setImageResource(R.drawable.a5);
-                    Toast.makeText(MainActivity.this, "Five",Toast.LENGTH_SHORT).show();
-                }
-                else if(rolledNo == 6){
+                    Toast.makeText(MainActivity.this, "Five", Toast.LENGTH_SHORT).show();
+                } else if (rolledNo == 6) {
                     imageView.setImageResource(R.drawable.a6);
-                    Toast.makeText(MainActivity.this, "Six",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Six", Toast.LENGTH_SHORT).show();
                 }
 
-               // Toast.makeText(MainActivity.this, "Rolled!",Toast.LENGTH_SHORT).show();
+                // Toast.makeText(MainActivity.this, "Rolled!",Toast.LENGTH_SHORT).show();
 
             }
         });
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater=new MenuInflater(this);
+        menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+
+    }
+    public boolean onOptionsItemSelected(MenuItem menuItem)
+    {
+        switch (menuItem.getItemId())
+        {
+            case R.id.item0:
+                Toast.makeText(MainActivity.this, "Diceflip 1.0", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item1:
+                Toast.makeText(MainActivity.this, "Sharing options not available", Toast.LENGTH_LONG).show();
+                return true;
+            default: return super.onOptionsItemSelected(menuItem);
+        }
+
     }
     public void onBackPressed()
     {
